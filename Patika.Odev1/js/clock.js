21212eb853459d051d10c727eaf09415b1714344
serhan uygur <seuygur55@gmail.com>
2023-06-05 00:27:41 +0300

    Clock Added

--- Patika.Odev1/js/clock.js
+++ Patika.Odev1/js/clock.js
@@ -6,3 +6,21 @@ myName.innerHTML=`${firstName}`
 }else{
     myName.innerHTML=`No Name`    
 }
+
+function showTime(){
+    
+    const tarih=new Date;
+
+    let saat = tarih.getHours();
+    let dakika = tarih.getMinutes();
+    let saniye = tarih.getSeconds();
+    
+    
+    let gun =["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+    let gunName = gun[tarih.getDay()]
+    
+    let clock=document.querySelector("#myClock");
+    clock.innerHTML = `${saat}:${dakika}:${saniye} ${gunName}`;
+}
+
+setInterval(zaman,1000);
